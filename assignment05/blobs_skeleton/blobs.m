823fa1efa450dd77ecefbdf9e823d6f408554775
Jens Metzner <jens.metzner@uni-konstanz.de>
2014-11-27 16:44:29 +0100

    5.2.c

--- assignment05/blobs_skeleton/blobs.m
+++ assignment05/blobs_skeleton/blobs.m
@@ -13,24 +13,17 @@ image = double(image) / 255.0;
 [h w] = size( image );
 
 % Factor between consecutive scales
-%k = 1+1/9-0.00586;
-k = 1+1/9;
+k = 1.3;
 % Base scale
-sigma = 3/k;
+sigma = 0.5;
 % Total number of scales
 N = 20;
 % Vector of all scales
 scales = 1:N;
-
 % TODO: replace each entry in the vector "scales" by the correct
 % standard deviation of the Gaussian for this scale
-
-scales(1) = sigma;
-for t = 2:N
-   %scales(t) = scales(t-1)*k;
-   scales(t) = scales(t-1)*k;
-end
-scales
+% EDIT: factor 
+scales = k.^(scales-1) * sigma 
 
 % Scale space placeholder, one image per layer
 scs = zeros( h, w, N );
@@ -43,17 +36,23 @@ dog = zeros( h, w, N-1 );
 % you can use this to speed up testing for a local maximum if you want,
 % completely optional
 dog_max = zeros( h, w, N-1 );
+dog_min = zeros( h, w, N-1 );
 
 % IMPORTANT: Gaussian kernel size must be the same for all scales
 % so use maximum necessary value (for largest std. dev.)
-sz = 150; % initialize with maximum necessary kernel size
+% initialize with maximum necessary kernel size
+% EDIT: Formel used in openCV to calculate sigma.
+sz = ceil(10 / 3 * (2 * scales(end) - 1))
+
 
 % Initialize scale space
 for t = 1:N
     %t
+    % compute Gaussian convolution of image with
+    % std. dev. for scale t, kernel size sz
+    % EDIT: create Gaussian
     kernel = fspecial('gaussian', sz, scales(t));
-    scs( :,:,t ) = imfilter(image, kernel); % compute Gaussian convolution of image with
-                         % std. dev. for scale t, kernel size sz
+    scs( :,:,t ) = imfilter(image, kernel,'replicate'); 
 end
 
 % super cool scale space visualization
@@ -63,10 +62,13 @@ figure(1), clf( 'reset' ), volume_visualize( 1:w, 1:h, 1:N, scs ), colormap gray
 % a 3x3 neighborhood - this makes checking for scale space
 % maxima slightly more efficient
 for t = 1:N-1
-    dog( :,:,t ) = scs(:,:,t+1) - scs(:,:,t);  % compute difference of Gaussians at this scale
-
+    % compute difference of Gaussians at this scale
+    % EDIT: 
+    dog( :,:,t ) = scs(:,:,t+1) - scs(:,:,t);  
     % this is optional, use if you like to speed up future tests
     dog_max( :,:,t ) = local_maxima_3x3( dog( :,:,t ));
+    % EDIT: also find local minimal
+    dog_min( :,:,t ) = -local_maxima_3x3( -dog( :,:,t ));
 end
     
 % equally cool DoG visualization
@@ -79,37 +81,33 @@ figure(2), clf( 'reset' ), volume_visualize( 1:w, 1:h, 1:N-1, dog ), colormap je
 
 % Note that local maxima only exist beween scale space layers,
 % the boundaries are ignored for the sake of simplicity.
-count = 1;
+count_maxima = 1;
+count_minima = 1;
 clear maxima;
+clear minima;
 for x=2:h-1
-    %x % feedback to see something is still happening
+    x % feedback to see something is still happening
       % if this loop gets very very slow the longer it runs, you are detecting
       % too many local maxima, i.e. probably have buggy code ;)
     for y=2:w-1
         for t=2:N-2
-            
             v = dog( x,y,t );
 
             % TODO: in next line, replace the random initialization with
             % the correct test for a local MAXIMUM (note: this only detects
-            % black blobs).            
-            %is_local_maximum = v < 0.01;
-            
-            %is_local_maximum = true;
-            
-            v_cube = ones(3,3,3)*v;
-            
-            is_bigger = dog(x-1:x+1,y-1:y+1,t-1:t+1) > v_cube;
-            is_local_maximum = sum(is_bigger(:)) == 0;
-            
+            % black blobs).
+            % keep the feature
+            % EDIT: 
+            is_local_maximum = v == dog_max(x,y,t);
+            is_local_minimum = v == dog_min(x,y,t);
             if ( is_local_maximum )
-                                 
-                 % keep the feature
-                 maxima( count,: ) = [ v y x scales(t) ];
-                 count = count + 1;
-
+                 maxima( count_maxima,: ) = [ v y x scales(t) ];
+                 count_maxima = count_maxima + 1;
             end
-            
+            if ( is_local_minimum )
+                 minima( count_minima,: ) = [ v y x scales(t) ];
+                 count_minima = count_minima + 1;
+            end
         end
     end
 end
@@ -120,15 +118,25 @@ end
 % Fth best feature as a threshold)
 F = 150;
 sorted_max = sort( maxima );
-threshold = sorted_max( size(maxima,1) - F, 1 );
+sorted_min = sort( minima );
+threshold_max = sorted_max( size(maxima,1) - F, 1 );
+threshold_min = sorted_min( F + 1, 1 );
 
 % Draw a circle into the image for each of the features
 figure(3), clf, imshow( image ), hold on;
 for m=1:size(maxima,1)
-    if ( maxima( m,1 ) > threshold )
+    if ( maxima( m,1 ) > threshold_max )
         x = maxima( m, 2 );
         y = maxima( m, 3 );
         r = maxima( m, 4 ) * sqrt(2.0);
         circle( [x y], r, 1000, '-y' );
+    end
+end
+for m=1:size(minima,1)
+    if ( minima( m,1 ) < threshold_min )
+        x = minima( m, 2 );
+        y = minima( m, 3 );
+        r = minima( m, 4 ) * sqrt(2.0);
+        circle( [x y], r, 1000, '-r' );
     end
 end
